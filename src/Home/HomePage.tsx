61edc0c8a8adb452b411d25db5f2cc459eddf8dc
Junho Yeo <hanaro0704@gmail.com>
2021-07-18 23:11:09 +0900

    클릭 수 표시

--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -10,7 +10,10 @@ export const HomePage = () => {
   return (
     <Container>
       <Card onClick={updateClicks} />
-      {clicks}
+      <Minted>{clicks.toLocaleString('ko')}번 돈을 찍어내셨네요.</Minted>
+      <Credits href="https://github.com/junhoyeo/pride-stimulus">
+        GitHub@junhoyeo
+      </Credits>
     </Container>
   );
 };
@@ -19,8 +22,22 @@ const Container = styled.div`
   width: 100%;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-image: url('/images/gradient.webp');
   background-size: cover;
+`;
+
+const Minted = styled.span`
+  margin-top: 16px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: rgba(33, 37, 41, 0.85);
+`;
+
+const Credits = styled.a`
+  margin-top: 8px;
+  font-weight: bold;
+  font-size: 0.98rem;
 `;
